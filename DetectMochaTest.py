ac8f97da8af354df3ac2715ec81e1364c9e5c1dc
Lennart Melzer <me@lmaa.name>
2012-04-01 19:16:34 +0200

    [add] mocha test file detection

---
+++ DetectMochaTest.py
@@ -0,0 +1,24 @@
+import sublime, sublime_plugin
+import os
+
+class MochaDetectFileTypeCommand(sublime_plugin.EventListener):
+  """ Detects test files written in js"""
+  """ Modified for Mocha tests and Sublime Text 2 """
+  """ Original Gist for Rails here: https://gist.github.com/925008 """
+
+  def on_load(self, view):
+    filename = view.file_name()
+    if not filename: # buffer has never been saved
+      return
+
+    name = os.path.basename(filename.lower())
+    dirname = os.path.dirname(filename.lower())
+    if dirname[-4:] == "test":
+      set_syntax(view, "Mocha", "Mocha")
+
+
+def set_syntax(view, syntax, path=None):
+  if path is None:
+    path = syntax
+  view.settings().set('syntax', 'Packages/'+ path + '/' + syntax + '.tmLanguage')
+  print "Switched syntax to: " + syntax
